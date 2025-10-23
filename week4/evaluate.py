7d83f9e18a5f780da59e9772f95e471a6afc7207
Kelly Ojukwu <kojukwu2003@gmail.com>
2025-10-22 23:42:10 -0700

    Init commit have local and global alignment

---
+++ week4/evaluate.py
@@ -0,0 +1,129 @@
+# from utilities.file_tools import Fasta
+import os
+import subprocess
+import zipfile
+import glob
+from datetime import datetime as dt
+from typing import List
+
+
+base_dir_path = os.path.dirname(__file__)
+week_data_dir = os.path.join(base_dir_path, 'data')
+
+
+# First compile the binary version using codon
+codon_binary_cmd_template = './test_codon'
+
+def calculate_N50(contig_len_list:list[int]) -> int:
+    total_len = sum(contig_len_list)
+    contig_len_list.sort(reverse=True)
+    
+    half_len = total_len/2
+    cur_len = 0
+    index = 0
+    for length in contig_len_list:
+        
+        cur_len += length
+        if cur_len >= half_len:
+            return length
+
+def create_contigs(data_dir:str):
+
+    # Create the contigs using the python interpreter
+    codon_run(data_dir)
+    python_run(data_dir)
+
+    pass
+
+def read_fasta(fasta_file:List[str]):
+    data = []
+    for line in fasta_file:
+        line = line.strip()
+        if line[0] != '>':
+            data.append(line)
+    # print(name, len(data), len(data[0]))
+    # print('Sample:', data[0])
+    return data
+
+
+def main():
+    # Firstly compile the binary for codon to avoid having to use codon run
+    source_code_dir_path = os.path.join(base_dir_path, 'code')
+    # run_setup(source_code_dir_path)
+    print(f"{'Method':<15}{'Language':<15} {'Runtime':>15}")
+    
+    codon_test_output = codon_run().splitlines()
+    python_test_output = python_run().splitlines()
+    for output in codon_test_output:
+        out_tuple = output.split("|")
+        print(f"{out_tuple[0]:<15}{out_tuple[1]:<15} {out_tuple[2]:>15}ms")
+    for output in python_test_output:
+        out_tuple = output.split("|")
+        print(f"{out_tuple[0]:<15}{out_tuple[1]:<15} {out_tuple[2]:>15}ms")
+
+
+        # print(f"{out_tuple[0]:<10} {python_test_output.strip():>6}ms")
+
+
+
+    # print(f"{'Language':<10} {'Runtime':>10}")
+    # print("-" * 21)
+    # print(f"{'python':<10} {python_test_output.strip():>6}ms")
+    # print(f"{'codon':<10} {codon_test_output.strip():>6}ms")
+
+
+
+
+    
+
+
+
+
+def get_contig_lengths(data_dir_path):
+    with open(os.path.join(base_dir_path,f'{data_dir_path}/contig.fasta')) as contig_file:
+        contigs = read_fasta(contig_file.readlines())
+        return list(map(len, contigs))
+    
+def python_run():
+    result = subprocess.run(f'cd test  && ulimit -s 8192000 && python test_alignment.py', capture_output=True, cwd= base_dir_path, check=True,shell=True, text=True)  
+    return(str(result.stdout))
+    # n_50 = calculate_N50(get_contig_lengths())
+    # return n_50
+
+def codon_run():
+    # print(base_dir_path)
+    subprocess.run(f'cd test && codon build -release test_alignment.codon -o test_alignment',cwd= base_dir_path, shell=True, check=True)
+
+    result = subprocess.run(f'cd test && ./test_alignment',cwd= base_dir_path, shell=True, check=True, capture_output=True, text=True)
+    return str(result.stdout)
+
+    
+
+
+def run_setup(source_code_dir_path):
+    '''
+    Unzip files and compile source code using codon and release flag
+    '''
+    # codon_compile_cmd = '~/.codon/bin/codon build -release code/codon_src/main.codon -o main_codon'
+    codon_compile_cmd = 'codon build -release code/codon_src/main.codon -o main_codon'
+
+    # try:
+    result = subprocess.run(f'pwd && {codon_compile_cmd}',  shell=True, check=True, cwd= base_dir_path, capture_output=True)
+    # except subprocess.CalledProcessError as e:
+    #     print(base_dir_path)
+    #     print(codon_compile_cmd)
+    #     print(e.stdout)
+    #     print(e.stderr)
+
+    data_zip_glob = 'data*.zip'
+    for zip_path in glob.glob(os.path.join(week_data_dir, data_zip_glob)):
+        os.makedirs(week_data_dir, exist_ok=True)
+
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(week_data_dir)
+
+
+
+main()
+
+ #TODO: Use codon bridge to run some utilities with the python interpreter 
---
+++ week4/test/test_alignment.py
@@ -0,0 +1,111 @@
+import re
+from datetime import datetime as dt
+start = dt.now()
+
+from week4.code.python.alignment import GlobalAlignment, LocalAlignment
+# simian_file_names = ["MT-human.fa", "MT-orang.fa"]
+# other_file_names = ['q1.fa', 'q2.fa', 't1.fa', 't2.fa']
+def gen_unique_pairs(my_list):
+    pairs = []
+    for i in range(len(my_list)):
+        for j in range(i, len(my_list)):
+            pairs.append((my_list[i], my_list[j]))
+    return pairs
+
+
+
+
+# print(gen_unique_pairs(other_file_names))
+
+def get_fasta_as_list(fasta_string: str) -> list:   
+# def get_fasta_as_list(fasta_file: Union[TextIO, str]= None, fasta_string= None) -> dict:   
+
+    # if fasta_string is not None:
+    #     string = fasta_string
+    # else:
+    #     if type(fasta_file) == TextIO:
+    #         string = fasta_file.read()
+    #     elif type(fasta_file) == str:
+    #         with open(fasta_file) as f_file:
+    #             string = f_file.read()
+
+    matches_list = re.split(">(.*)", fasta_string)[1:] # Skip the blankspace match
+
+    
+    sequence_list = list(map(lambda sequence: re.sub(r"\s", "", sequence), matches_list[1::2]))
+    
+    return sequence_list
+
+
+
+def test_global_alignment():
+    query_fasta_list = []
+    t_fasta_list = []
+    with open("data/q1.fa") as query_fasta_file:
+        query_fasta_list = get_fasta_as_list(query_fasta_file.read().upper())
+    with open("data/t1.fa") as t_fasta_file:
+        t_fasta_list = get_fasta_as_list(t_fasta_file.read().upper())
+    for i in range(0, len(query_fasta_list)):
+        q_fasta = query_fasta_list[i]
+        t_fasta = t_fasta_list[i]
+        start = dt.now()
+        alignment = GlobalAlignment(q_fasta, t_fasta)
+        end = dt.now()
+        run_time = str(round((end-start).total_seconds()*1000, 2))
+        print(f"global-q{i}|python|{run_time}|")    
+    
+    # human_fasta = ""
+    # orang_fasta = ""
+    # with open("data/MT-human.fa") as human_fasta_file:
+    #     human_fasta = get_fasta_as_list(human_fasta_file.read().upper())[0]
+    # with open("data/MT-orang.fa") as tang_fasta_file:
+    #     orang_fasta = get_fasta_as_list(tang_fasta_file.read().upper())[0]
+    
+    # start = dt.now()
+    # alignment_two = GlobalAlignment(human_fasta, orang_fasta)
+    # end = dt.now()
+    # print((end-start).total_seconds()*1000)
+    # run_time = str(round((end-start).total_seconds()*1000, 2))
+    # print(f"global-mt_human|codon|{run_time}|")
+
+
+    # for i in range(len(my_list)):
+    #     for j in range(i, len(my_list)):
+def test_local_alignment():
+    query_fasta_list = []
+    t_fasta_list = []
+    with open("data/q1.fa") as query_fasta_file:
+        query_fasta_list = get_fasta_as_list(query_fasta_file.read().upper())
+    with open("data/t1.fa") as t_fasta_file:
+        t_fasta_list = get_fasta_as_list(t_fasta_file.read().upper())
+    for i in range(0, len(query_fasta_list)):
+        q_fasta = query_fasta_list[i]
+        t_fasta = t_fasta_list[i]
+        start = dt.now()
+        alignment = LocalAlignment(q_fasta, t_fasta)
+        end = dt.now()
+        run_time = str(round((end-start).total_seconds()*1000, 2))
+        print(f"local-q{i}|codon|{run_time}|")    
+    human_fasta = ""
+    orang_fasta = ""
+    with open("data/MT-human.fa") as human_fasta_file:
+        human_fasta = get_fasta_as_list(human_fasta_file.read().upper())[0]
+    with open("data/MT-orang.fa") as tang_fasta_file:
+        orang_fasta = get_fasta_as_list(tang_fasta_file.read().upper())[0]
+    # start = dt.now()
+    # alignment_two = LocalAlignment(human_fasta, orang_fasta)
+    # end = dt.now()
+    # run_time = str((end-start).total_seconds()*1000)
+    # print(f"local-mt_human|codon|{run_time}|")#(end-start).total_seconds()*1000)
+
+
+    # for i in range(len(my_list)):
+    #     for j in range(i, len(my_list)):
+
+
+
+test_global_alignment()
+test_local_alignment()
+
+#     pass
+
